f3c8420481500ee55a9b18f1d4049a59710d467e
SiogunJH <homarofficial@gmail.com>
2023-10-30 00:56:17 +0100

    Basic sound + Code Cleanup

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -9,21 +8,26 @@ public class PlayerLogic : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sr;
     Animator anim;
-    public GameObject particleGO;
+    [SerializeField] GameObject particleGO;
+    [SerializeField] AudioClip[] softLandingAudio;
+    [SerializeField] AudioClip[] hardLandingAudio;
+    [SerializeField] AudioClip[] wallBonkAudio;
+    [SerializeField] AudioClip[] jumpAudio;
+    AudioSource audioSource;
     ParticleSystem ps;
-    public bool isOnGround;
+    bool isOnGround;
     float jumpHorizontal;
     float jumpVertical;
     float jumpStrength;
     Direction facing;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
         ps = particleGO.GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
 
         facing = Direction.Right;
         jumpHorizontal = 6.5f;
@@ -34,10 +38,17 @@ public class PlayerLogic : MonoBehaviour
         Physics2D.gravity = new Vector2(0.00f, -20.00f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Direction Inputs
+        UpdateDirection(); // Direction Inputs        
+        JumpLogic(); // Jump Logic
+
+        anim.SetFloat("Vertical Speed", rb.velocity.y); // Update Animation
+    }
+
+    // Listen for LEFT and RIGHT inputs, and change player Direction accordingly
+    void UpdateDirection()
+    {
         if (Input.GetKeyDown(KeyCode.A) && isOnGround && facing == Direction.Right)
         {
             facing = Direction.Left;
@@ -49,8 +60,13 @@ public class PlayerLogic : MonoBehaviour
             facing = Direction.Right;
             sr.flipX = false;
         }
+    }
 
-        // Jump Logic
+    // When on ground, listen for JUMP input
+    // Holding down JUMP input results in higher jumpStrength, but it cannot exceed 1
+    // Minimum jumpStrength is 0.1
+    void JumpLogic()
+    {
         if (isOnGround)
         {
             if (Input.GetKeyUp(KeyCode.Space) || jumpStrength > 1)
@@ -63,13 +79,12 @@ public class PlayerLogic : MonoBehaviour
                 jumpStrength += Time.deltaTime;
             }
         }
-
-        // Update Animation
-        anim.SetFloat("Vertical Speed", rb.velocity.y);
     }
 
+    // Jump in predetermined direction, based on player Direction
     void Jump()
     {
+        // Jump in a direction
         if (facing == Direction.Left)
         {
             rb.velocity = new Vector2(-jumpHorizontal * jumpStrength, jumpVertical * jumpStrength);
@@ -78,6 +93,42 @@ public class PlayerLogic : MonoBehaviour
         {
             rb.velocity = new Vector2(jumpHorizontal * jumpStrength, jumpVertical * jumpStrength);
         }
+
+        // Play jump sound
+        audioSource.clip = jumpAudio[Random.Range(0, jumpAudio.Length)];
+        audioSource.Play();
+    }
+
+    // Set isOnGround and update animation variable
+    void OnLanding(bool isLandingSoft)
+    {
+        // Set "Is on Ground"
+        isOnGround = true;
+        anim.SetBool("Is on Ground", isOnGround);
+
+        if (!isLandingSoft)
+        {
+            // Generate particles
+            particleGO.transform.position = new Vector2(transform.position.x, transform.position.y - 0.6f);
+            ps.Play();
+
+            // Play hard landing sound
+            audioSource.clip = hardLandingAudio[Random.Range(0, hardLandingAudio.Length)];
+            audioSource.Play();
+        }
+        else
+        {
+            // Play soft landing sound
+            audioSource.clip = softLandingAudio[Random.Range(0, softLandingAudio.Length)];
+            audioSource.Play();
+        }
+    }
+
+    void OnBounce()
+    {
+        // Play wall bonk sound
+        audioSource.clip = wallBonkAudio[Random.Range(0, wallBonkAudio.Length)];
+        audioSource.Play();
     }
 
     // On Triggers
@@ -88,27 +139,20 @@ public class PlayerLogic : MonoBehaviour
     // On Collisions
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // When the player hits the Standable object and stops moving
         if (collision.gameObject.CompareTag("Standable") && rb.velocity.magnitude < 0.01)
         {
-            // Set "Is on Ground"
-            isOnGround = true;
-            anim.SetBool("Is on Ground", isOnGround);
-
-            // Apply actions below only when player hits the Standable with 10+ velcity
-            if (collision.relativeVelocity.y > 10)
-            {
-                particleGO.transform.position = new Vector2(transform.position.x, transform.position.y - 0.6f);
-                ps.Play();
-            }
+            OnLanding(collision.relativeVelocity.y < 10);
+        }
+        else if (collision.gameObject.CompareTag("Bounceable"))
+        {
+            OnBounce();
         }
     }
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Standable") && !isOnGround && rb.velocity.magnitude < 0.01f)
         {
-            isOnGround = true;
-            anim.SetBool("Is on Ground", isOnGround);
+            OnLanding(false);
         }
     }
     void OnCollisionExit2D(Collision2D collision)
