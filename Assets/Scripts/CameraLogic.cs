9b227804b5bc792e681bef10bb79f97a61683d6a
SiogunJH <homarofficial@gmail.com>
2023-11-01 15:04:04 +0100

    Animated Tiles

--- Assets/Scripts/CameraLogic.cs
+++ Assets/Scripts/CameraLogic.cs
@@ -6,18 +6,23 @@ using UnityEngine;
 public class CameraLogic : MonoBehaviour
 {
     public GameObject playerGO;
-    PlayerLogic pl;
+    const float cameraSpeed = 3.75f;
     const float verticalFieldOfView = 6.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        pl = playerGO.GetComponent<PlayerLogic>();
-    }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        // Manual adjustment
+        if (Input.GetKey(KeyCode.W) && transform.position.y - playerGO.transform.position.y < verticalFieldOfView / 2.0f)
+        {
+            transform.position = new Vector3(0, transform.position.y + Time.deltaTime * cameraSpeed, -8.5f);
+        }
+        else if (Input.GetKey(KeyCode.S) && playerGO.transform.position.y - transform.position.y < verticalFieldOfView / 2.0f)
+        {
+            transform.position = new Vector3(0, transform.position.y - Time.deltaTime * cameraSpeed, -8.5f);
+        }
+
+        // Keep player in frame
         if (playerGO.transform.position.y - transform.position.y > verticalFieldOfView / 2.0f)
         {
             transform.position = new Vector3(0, playerGO.transform.position.y - (verticalFieldOfView / 2.0f), -8.5f);
